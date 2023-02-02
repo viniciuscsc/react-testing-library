5ff86f2ae6af509f296dfdf6fcbf23cfea10c336
cunhavcs <cunha.vcs@gmail.com>
2023-02-02 16:57:54 -0300

    Requisito 03 concluído

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 import FavoritePokemon from '../pages/FavoritePokemon';
 import renderWithRouter from '../renderWithRouter';
 
@@ -10,13 +12,25 @@ describe('Testes do componente FavoritePokemon', () => {
     () => {
       renderWithRouter(<FavoritePokemon />);
 
-      const tituloFavoritePokemon = screen.getByText('No favorite Pokémon found');
+      const fraseFavoritePokemon = screen.getByText('No favorite Pokémon found');
 
-      expect(tituloFavoritePokemon).toBeInTheDocument();
+      expect(fraseFavoritePokemon).toBeInTheDocument();
     },
   );
 
   it('Verifica se apenas são exibidos os Pokémon favoritados', () => {
-    
+    renderWithRouter(<App />);
+
+    const linkMaisDetalhes = screen.getByText('More details');
+    userEvent.click(linkMaisDetalhes);
+
+    const checkboxPokemonFavorito = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(checkboxPokemonFavorito);
+
+    const linkPkemonsFavoritos = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(linkPkemonsFavoritos);
+
+    const nomePokemon = screen.getByTestId('pokemon-name');
+    expect(nomePokemon).toBeInTheDocument();
   });
 });
