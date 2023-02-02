d55b611326d9d4e1183a92e335f87c375f3073e5
cunhavcs <cunha.vcs@gmail.com>
2023-02-02 15:43:55 -0300

    Requisito 03 em andamento

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente FavoritePokemon', () => {
+  it(
+    'Verifica se é exibida a mensagem "No favorite pokemon found", '
+    + 'caso a pessoa não tenha Pokémon favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemon />);
+
+      const tituloFavoritePokemon = screen.getByText('No favorite Pokémon found');
+
+      expect(tituloFavoritePokemon).toBeInTheDocument();
+    },
+  );
+
+  it('Verifica se apenas são exibidos os Pokémon favoritados', () => {
+    
+  });
+});
